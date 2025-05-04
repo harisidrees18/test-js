80a75af9a29237b49b7e96779bc421717549a061
harisidrees18 <harismuhammad482@gmail.com>
2025-05-04 18:31:52 +0000

    operations

--- 01_Basic/03_conversion.js
+++ 01_Basic/03_conversion.js
@@ -1,12 +1,12 @@
 let my = "null"
 
-console.log(typeof my);
+// console.log(typeof my);
 
-console.log(typeof (my));
+// console.log(typeof (my));
 
 let value = Number(my)
-console.log(typeof value);
-console.log(value);
+// console.log(typeof value);
+// console.log(value);
 
 // "50" in form of string is easily converted to number 50//
 
@@ -15,8 +15,48 @@ console.log(value);
 //boolean value true is converted top 1
 let h = 5
 let b = Boolean(h)
-console.log(b);
+// console.log(b);
 
 //1 =. true 0=> false//
 // "" an empty string is also converted to false
 // if string has some value it is converted to true
+
+let somenumber= 33
+
+let stringNumber = String(somenumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *******************************Operations*******************************//
+ value = 5
+let negvalue = -value
+// console.log(negvalue);
+
+// /string additions
+let str1 = "hello"
+let str2 = "world"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 3);
+
+
+// console.log(+true);
+
+let num1, num2, num3
+
+ num1 = num2 = num3 = 2 + 2
+
+ console.log(num1);
+
+ let gameCounter = 90
+
+ gameCounter++
+
+ console.log(gameCounter);
+ 
+
+ 
+

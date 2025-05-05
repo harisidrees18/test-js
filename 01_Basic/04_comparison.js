d0231f165824d43eb05becd9f72d45863e5c71d1
harisidrees18 <harismuhammad482@gmail.com>
2025-05-05 05:36:53 +0000

    comparison

---
+++ 01_Basic/04_comparison.js
@@ -0,0 +1,16 @@
+// console.log("2">1);
+// console.log(1>"02");     this is not the right way to compare , always compare same datatypes
+
+console.log(null>0);
+console.log(null==0);
+console.log(null<=0);
+
+
+console.log(undefined < 0 );
+console.log(undefined == 0)
+console.log(undefined <= 0);
+;
+
+
+
+

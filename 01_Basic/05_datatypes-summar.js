61e5a808de9cebff05330585625b46ae80b5208a
harisidrees18 <harismuhammad482@gmail.com>
2025-05-05 07:53:24 +0000

    Datatypes Summarize

---
+++ 01_Basic/05_datatypes-summar.js
@@ -0,0 +1,44 @@
+// Primitive
+
+// types 7 : String , Number , Boolean , Null, Undefined, Symbol , BigInt
+// let id = Symbol('125')
+// let anotherId = Symbol('125')
+const id = Symbol('haris')
+const anotherId = Symbol('kinza')
+const value = 100
+const myValue = 100.34
+const outTemp = null
+const bigNumber = 356789905056n
+
+
+// console.log(id);
+
+// console.log(anotherId);
+
+// console.log(id===anotherId);
+
+// Refference type datatype
+
+// Arrays, Objects, Functions
+
+// arrays are written in sqaure brackets
+ const Ids = ["batman","superman", "ironman" ] ;
+
+ let myobj = {
+    name: "haris",
+    age: 27
+ }
+
+ const myFunction= function(){
+    console.log("Hello World");
+ }
+ console.log(typeof anotherId);
+ console.log(typeof myValue);
+console.log(typeof bigNumber);
+console.log(typeof outTemp);
+console.log(typeof myFunction);
+
+
+
+ 
+ 
